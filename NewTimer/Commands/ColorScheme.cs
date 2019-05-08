7e870d038dbe87201371a2e0bbd2834816cc6f4a
Kryxzael <kasper.ramskjell98@gmail.com>
2019-05-08 15:47:21 +0200

    'Colorscheme' command now lets you create a custom solid colorscheme

--- NewTimer/Commands/ColorScheme.cs
+++ NewTimer/Commands/ColorScheme.cs
@@ -18,7 +18,8 @@ namespace NewTimer.Commands
         public override Syntax GetSyntax(Params args)
         {
             return Syntax.Begin().Or()
-                .Add("Index", Range.From(0).To(Config.ColorSchemes.GetUpperBound(0)), true);
+                .Add("Index", Range.From(0).To(Config.ColorSchemes.GetUpperBound(0)), true).Or()
+                .Add("R", Range.From(0).To(255), true).Add("G", Range.From(0).To(255), true).Add("B", Range.From(0).To(255), true);
         }
 
         protected override void Executed(Params args, IConsoleOutput target)
@@ -27,15 +28,31 @@ namespace NewTimer.Commands
             if (args.Count == 0)
             {
                 target.WriteLine("[" + Config.ColorSchemes.TakeWhile(i => i != Config.ColorScheme).Count() + "] " + Config.ColorScheme.Name);
+                return;
             }
 
             //Write the color scheme
-            else
+            else if (args.Count == 1)
             {
                 Config.ColorScheme = Config.ColorSchemes[args.ToInt(0)];
-                Config.ColorizeTimerBar();
-                target.WriteLine("Color scheme has been updated to '" + Config.ColorScheme.Name + "'");
             }
+
+            //Write color directly
+            else
+            {
+                Config.ColorScheme = new Schemes.SchemeCustom(
+                    name: "$custom_scheme", 
+                    looptype: Schemes.SchemeCustom.LoopType.Ceiling, 
+                    colors: System.Drawing.Color.FromArgb(
+                        red: args.ToInt(0), 
+                        green: args.ToInt(1), 
+                        blue: args.ToInt(2)
+                    )
+                );
+            }
+
+            Config.ColorizeTimerBar();
+            target.WriteLine("Color scheme has been updated to '" + Config.ColorScheme.Name + "'");
         }
     }
 }
